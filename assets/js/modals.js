7159b3f5659e0d47cf244cd3f0bb1a6a3419ec56
Jose Lorenzo L. Victorino <josevictorino003@gmail>
2025-03-25 17:00:33 +0800

    structual changes <modals>

--- assets/js/modals.js
+++ assets/js/modals.js
@@ -1,82 +1,56 @@
-/**
- * @typedef {'text' | 'number' | 'email' | 'option'} Input
- * @typedef {'check' | 'info' | 'warning' | 'error' | 'question'} Icon
- * @typedef {'top-left' | 'bottom-left' | 'top-right' | 'bottom-right' | 'center'} Locations
- * 
- * @typedef {Object} AlertPN
- * @property {string} title - The title of the alert.
- * @property {string} text - The alert message.
- * @property {Icon} icon - The type of icon to display.
- * @property {Locations} [location] - The location of the alert on the screen (default: 'top-left').
- * @property {boolean} [animate] - Enable alert animation (default: true).
- * @property {boolean} [autoClose] - Toggle auto close (default: true).
- * @property {number} [timer] - Duration (in milliseconds) before the alert disappears (default: 2000).
- * 
- * @typedef {Object} InputPN
- * @property {string} title - The title of the input modal.
- * @property {string} text - The input prompt message.
- * @property {Icon} icon - The type of icon to display.
- * @property {Input} input - The type of input field.
- * @property {Array} options - List of options.
- * @property {string} [placeholder] - Placeholder text for the input field.
- * @property {Array<string>} options - Options for a select input (if applicable).
- * 
- * @typedef {Object} ConfirmPN
- * @property {string} title - The title of the confirmation modal.
- * @property {string} text - The confirmation prompt message.
- * @property {Icon} icon - The type of icon to display.
- * @property {string} [confirmButtonText] - Text for the confirm button.
- * @property {string} [cancelButtonText] - Text for the cancel button.
- * 
- * @typedef {Object} Results
- * @property {Boolean} isConfirmed - User confirmed input
- * @property {Boolean} isCancelled - User cancelled input
- */
-class BimPN {
-  constructor(){}
-  #delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
-  #init(){
-    const divPN = document.createElement('div');
-    document.body.appendChild(divPN);
-    divPN.innerHTML = `
-    <article>
-      <section class="content">
-        <img class="icon center" alt="icon">
-        <div class="info">
-          <img class="icon side" alt="icon">
-          <div class="innerCont">
-            <h2 class="title"></h2>
-            <p class="text"></p>
-            <input required>
-            <select></select>
-            <span></span>
-          </div>
-        </div>
-      </section>
-      <section class="mainBtn">
-        <button class="confirm"></button>
-        <button class="cancel"></button>
-      </section>
-    </article>`;
-    const confirm = divPN.querySelector('.confirm');
-    const cancel = divPN.querySelector('.cancel');
-    const sideImg = divPN.querySelector('.icon.side');
-    const centerImg = divPN.querySelector('.icon.center');
-    const titleB = divPN.querySelector('.title');
-    const textP = divPN.querySelector('.text');
-    const inputTag = divPN.querySelector('input');
-    const selectTag = divPN.querySelector('select');
-    const span = divPN.querySelector('span');
-    const icons = {
-      check: './assets/images/svg/circle-check.svg',
-      info: './assets/images/svg/circle-info.svg',
-      warning: './assets/images/svg/circle-exclamation.svg',
-      error: './assets/images/svg/circle-xmark.svg',
-      question: './assets/images/svg/circle-question.svg',
-    }
-    return {divPN, confirm, cancel, sideImg, centerImg, titleB, textP, inputTag, selectTag, span, icons};
+const bimPN = (() => {
+  const icons = {
+    check: './assets/images/svg/circle-check.svg',
+    info: './assets/images/svg/circle-info.svg',
+    warning: './assets/images/svg/circle-exclamation.svg',
+    error: './assets/images/svg/circle-xmark.svg',
+    question: './assets/images/svg/circle-question.svg',
   }
-  #validate(el, data, arr){
+  /**
+   * @typedef {'text' | 'number' | 'email' | 'option'} Type
+   * @typedef {'check' | 'info' | 'warning' | 'error' | 'question'} Icon
+   * @typedef {'top-left' | 'bottom-left' | 'top-right' | 'bottom-right' | 'center'} Locations
+   * @typedef {'alert' | 'input' | 'confirm'} ModalType
+   * 
+   * @typedef {Object} Results
+   * @property {Boolean} isConfirmed - User confirmed input
+   * @property {Boolean} isCancelled - User cancelled input
+   */
+  /**
+   * @param {Number} ms 
+   * @returns {Promise<Results>}
+   */
+  const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+  /**
+   * @param {ModalType} modalType 
+   * @param {ModalData} data 
+   */
+  const setDefaultValues = (modalType, data) => {
+    data.modalType = modalType;
+    switch(modalType){
+      case 'alert':
+        data.location ??= 'top-left';
+        data.animate ??= true;
+        data.autoClose ??= true;
+        data.timer = (data.timer ?? 2000) + 200;
+      break;
+      case 'input':
+        data.location = 'center';
+        data.type ??= 'text';
+        data.placeholder ??= '';
+        data.animate ??= false;
+      break;
+      case 'confirm':
+        data.location = 'center';
+        data.animate ??= false;
+      break;
+    }
+  };
+  /**
+   * @param {ModalData} data
+   * @param {Array<string>} arr
+   */
+  const validate = (data, arr) => {
     Object.entries(data).forEach(([key, value]) => {
       switch(key){
         case 'timer':
@@ -88,7 +62,7 @@ class BimPN {
             throw new Error(`Invalid options: '${value}', options must be an array.`);
           break;
         case 'icon':
-          if(!Object.keys(el.icons).includes(value))
+          if(!Object.keys(icons).includes(value))
             throw new Error(`Invalid icon: '${value}'.`);
           break;
         case 'animate':
@@ -107,17 +81,127 @@ class BimPN {
     });
     
     arr.forEach(item => {
+      item
       if(data[item] !== undefined) return;
       throw new Error(`${item} is not defined.`);
     });
-  }
-  async #setElements(el, data){ 
-    let {modalType, title, text, icon, location, animate, autoClose, timer, confirmBtnText, cancelBtnText} = data;
+  };
+  /**
+   * @param {HTMLElement} parent 
+   * @param {String} tagName 
+   * @param {String | Array<String> | undefined} classNames 
+   * @returns {HTMLElement}
+   */
+  const createSetElement = (parent, tagName, classNames) => {
+    const child = document.createElement(tagName);
+    if(Array.isArray(classNames)){
+      child.classList.add(...classNames);
+    }
+    else if(typeof classNames === 'string'){
+      child.classList.add(classNames);
+    }
+    parent.append(child);
     
-    el.titleB.innerText = title || '';
-    el.textP.innerText = text;
-    el.confirm.innerText = confirmBtnText || 'Confirm';
-    el.cancel.innerText = cancelBtnText || 'Cancel';
+    return child;
+  };
+  /**
+   * @param {ModalData} data 
+   * @returns {Object<string, HTMLElement>}
+   */
+  const init = ({modalType, location, icon, type, autoClose}) => {    
+    const divPN = createSetElement(document.body, 'div');
+    const article = createSetElement(divPN, 'article');
+    let content, info, sideImg, centerImg, titleB, textP, inputTag, selectTag, span, btnCont, confirm, cancel;
+
+    switch(modalType){
+      case 'alert':
+        content = createSetElement(article, 'section', 'content');
+
+        if(location === 'center'){
+          if(icon) centerImg = createSetElement(content, 'img', ['icon', 'center']);
+          info = createSetElement(content, 'div', 'info');
+        }
+        else{
+          info = createSetElement(content, 'div', 'info');
+          if(icon) sideImg = createSetElement(info, 'img', ['icon', 'side']);
+        }
+        
+        TinnerCont = createSetElement(info, 'div', 'innerCont');
+
+        if(location === 'center'){
+          titleB = createSetElement(TinnerCont, 'h2', 'title');
+          if(!autoClose){
+            btnCont = createSetElement(article, 'section', 'mainBtn');
+            confirm = createSetElement(btnCont, 'button', 'confirm');
+          }
+        }
+        textP = createSetElement(TinnerCont, 'p', 'text');
+      break;
+      case 'input':
+        content = createSetElement(article, 'section', 'content');
+        info = createSetElement(content, 'div', 'info');
+        TinnerCont = createSetElement(info, 'div', 'innerCont');
+        textP = createSetElement(TinnerCont, 'p', 'text');
+        if(['text', 'number', 'email'].includes(type)){
+          inputTag = createSetElement(TinnerCont, 'input');
+          span = createSetElement(TinnerCont, 'span');
+        }
+        else if(type === 'option'){
+          selectTag = createSetElement(TinnerCont, 'select');
+        }
+        btnCont = createSetElement(article, 'section', 'mainBtn');
+        confirm = createSetElement(btnCont, 'button', 'confirm');
+        cancel = createSetElement(btnCont, 'button', 'cancel');
+      break;
+      case 'confirm':
+        content = createSetElement(article, 'section', 'content');
+        if(icon) centerImg = createSetElement(content, 'img', ['icon', 'center']);
+
+        info = createSetElement(content, 'div', 'info');
+        TinnerCont = createSetElement(info, 'div', 'innerCont');
+        titleB = createSetElement(TinnerCont, 'h2', 'title');
+        textP = createSetElement(TinnerCont, 'p', 'text');
+        
+        btnCont = createSetElement(article, 'section', 'mainBtn');
+        confirm = createSetElement(btnCont, 'button', 'confirm');
+        cancel = createSetElement(btnCont, 'button', 'cancel');
+      break;
+    }
+
+    // divPN.innerHTML =
+    // `<article>
+    //   <section class="content">
+    //     <img class="icon center" alt="icon">
+    //     <div class="info">
+    //       <img class="icon side" alt="icon">
+    //       <div class="innerCont">
+    //         <h2 class="title"></h2>
+    //         <p class="text"></p>
+    //         <input required>
+    //         <select></select>
+    //         <span></span>
+    //       </div>
+    //     </div>
+    //   </section>
+    //   <section class="mainBtn">
+    //     <button class="confirm"></button>
+    //     <button class="cancel"></button>
+    //   </section>
+    // </article>`;
+    return {divPN, article, content, info, sideImg, centerImg, titleB, textP, inputTag, selectTag, span, btnCont, confirm, cancel};
+  };
+  /**
+   * @param {Object<string, HTMLElement>} el 
+   * @param {Object} data 
+   */
+  const setElements = async (el, data) => { 
+    const {modalType, title, text, icon, location, type, placeholder, options, confirmBtnText, cancelBtnText, animate, autoClose, timer} = data;
+    const {divPN, confirm, cancel, sideImg, centerImg, titleB, textP, inputTag, selectTag, span} = el;
+    
+    textP.innerText = text;
+    if(titleB) titleB.innerText = title || '';
+    if(confirm) confirm.innerText = confirmBtnText || 'Confirm';
+    if(cancel) cancel.innerText = cancelBtnText || 'Cancel';
     
     Object.assign(el.divPN.dataset, {
       bimpn: '',
@@ -126,19 +210,23 @@ class BimPN {
     });
 
     if(icon){
-      el.sideImg.src = el.icons[icon];
-      el.centerImg.src = el.icons[icon];
-
-      el.divPN.dataset.bimpnHasimg  = '';
+      if(sideImg){
+        sideImg.src = icons[icon];
+        sideImg.alt = 'icon';
+      }
+      if(centerImg){
+        centerImg.src = icons[icon];
+        centerImg.alt = 'icon';
+      }
     }
 
     if(animate) el.divPN.dataset.bimpnAnimate = 'start';
     if(modalType === 'alert'){
       if(autoClose || location !== 'center'){
-        await this.#delay(timer);
+        await delay(timer);
         if(animate) el.divPN.dataset.bimpnAnimate = 'end';
-        await this.#delay(180);
-        document.body.removeChild(el.divPN);
+        await delay(180);
+        document.body.removeChild(divPN);
       }
       else{
         el.divPN.dataset.bimpnAutoClose = '';
@@ -146,15 +234,32 @@ class BimPN {
         el.confirm.addEventListener('click', async () => {
           if(animate){
             el.divPN.dataset.bimpnAnimate = 'end';
-            await this.#delay(180);
+            await delay(180);
           }
-          document.body.removeChild(el.divPN);
+          document.body.removeChild(divPN);
         });
       }
     }
-    
-  }
-  #buttonEvents(el, type, callback){
+    else if(modalType === 'input'){
+      if(['text', 'number', 'email'].includes(type)){
+        Object.assign(el.inputTag, {
+          type,
+          placeholder,
+          required: true,
+        });
+      }
+      else if(type === 'option'){
+        for(const option of options)
+          el.selectTag.innerHTML += `<option value="${option}">${option}</option>`;
+      }
+    }
+  };
+  /**
+   * @param {Object<string, HTMLElement>} el 
+   * @param {String} type  
+   * @param {(result: Results) => void} callback
+   */
+  const buttonEvents = (el, type, callback) => {
     el.divPN.addEventListener('click', e => {
       const tArticle = e.target.closest('article');
       const tButton = e.target.closest('button');
@@ -178,89 +283,94 @@ class BimPN {
         el.span.innerText = 'Invalid Input';
         el.span.style.display = 'inline';
       }
-    })
-  }
-  /**
-   * @param {AlertPN} data
-   */
-  alert(data){
-    data.modalType = 'alert';
-    data.location ??= 'top-left';
-    data.animate ??= true;
-    data.autoClose ??= true;
-    data.timer = (data.timer ?? 2000) + 200;
-
-    const el = this.#init();
-    
-    this.#validate(el, data, ['text']);
-    this.#setElements(el, data);
-  };
-  /**
-   * @param {InputPN} data
-   * @returns {Promise<Results>}
-   */
-  async input(data){
-    return new Promise((resolve) => {
-      const {type = 'text', placeholder = '', options} = data;
-      data.modalType = 'input';
-      data.location = 'center';
-      data.animate ??= false;
-    
-      const el = this.#init();
-      
-      if(['text', 'number', 'email'].includes(type)){
-        this.#validate(el, data, ['text']);
-
-        Object.assign(el.inputTag, {type, placeholder});
-        el.inputTag.style.display = 'block';
-      }
-      else if(type === 'option'){
-        this.#validate(el, data, ['text', 'options']);
-
-        for(const option of options)
-          el.selectTag.innerHTML += `<option value="${option}">${option}</option>`;
-        
-        el.selectTag.style.display = 'block';
-      }
-      else throw new Error(`Invalid input type: '${type}'.`);
-      
-      this.#setElements(el, data);
-      
-      this.#buttonEvents(el, type, async (result) => {
-        if(result) resolve(result);
-        if(data.animate){
-          el.divPN.dataset.bimpnAnimate = 'end';
-          await this.#delay(180);
-        }
-        document.body.removeChild(el.divPN);
-      });
     });
   };
-  /**
-   * @param {ConfirmPN} data
-   * @returns {Promise<Results>}
-   */
-  async confirm(data){
-    return new Promise((resolve) => {
-      data.modalType = 'confirm';
-      data.location = 'center';
-      data.animate ??= false;
-    
-      const el = this.#init();
+  
+  class BimPushNotifications {
+    constructor(){}
+    /**
+     * @typedef {Object} AlertPN
+     * @property {string} title - The title of the alert.
+     * @property {string} text - The alert message.
+     * @property {Icon} icon - The type of icon to display.
+     * @property {Locations} [location] - The location of the alert on the screen (default: 'top-left').
+     * @property {boolean} [animate] - Enable alert animation (default: true).
+     * @property {boolean} [autoClose] - Toggle auto close (default: true).
+     * @property {number} [timer] - Duration (in milliseconds) before the alert disappears (default: 2000).
+     * 
+     * @param {AlertPN} data
+     */
+    alert = (data) => {
+      setDefaultValues('alert', data);
+      validate(data, ['text']);
+  
+      const el = init(data);
+      setElements(el, data);
+    };
+    /**
+     * @typedef {Object} InputPN
+     * @property {string} title - The title of the input modal.
+     * @property {string} text - The input prompt message.
+     * @property {Icon} icon - The type of icon to display.
+     * @property {Type} type - The type of input field.
+     * @property {Array} options - List of options.
+     * @property {string} [placeholder] - Placeholder text for the input field.
+     * @property {Array<string>} options - Options for a select input (if applicable).
+     * 
+     * @param {InputPN} data
+     * @returns {Promise<Results>}
+     */
+    input = async (data) => {
+      setDefaultValues('input', data);
       
-      this.#validate(el, data, ['text']);
-      this.#setElements(el, data);
+      (['text', 'number', 'email'].includes(data.type)) ? validate(data, ['text']) :
+      (data.type === 'option') ? validate(data, ['text', 'options']) :
+      (() => {throw new Error(`Invalid input type: '${data.type}'.`)})();
       
-      this.#buttonEvents(el, 'confirm', async (result) => {
-        if(result) resolve(result);
-        if(data.animate){
-          el.divPN.dataset.bimpnAnimate = 'end';
-          await this.#delay(180);
-        }
-        document.body.removeChild(el.divPN);
+      const el = init(data);
+      setElements(el, data);
+      
+      return new Promise((resolve) => {
+        buttonEvents(el, data.type, async (result) => {
+          if(result) resolve(result);
+          if(data.animate){
+            el.divPN.dataset.bimpnAnimate = 'end';
+            await delay(180);
+          }
+          document.body.removeChild(el.divPN);
+        });
       });
-    });
+    };
+    /**
+     * @typedef {Object} ConfirmPN
+     * @property {string} title - The title of the confirmation modal.
+     * @property {string} text - The confirmation prompt message.
+     * @property {Icon} icon - The type of icon to display.
+     * @property {string} [confirmButtonText] - Text for the confirm button.
+     * @property {string} [cancelButtonText] - Text for the cancel button.
+     * 
+     * @param {ConfirmPN} data
+     * @returns {Promise<Results>}
+     */
+    confirm = async (data) => {
+      setDefaultValues('confirm', data);
+      validate(data, ['text']);
+  
+      const el = init(data);
+      setElements(el, data);
+  
+      return new Promise((resolve) => {
+        buttonEvents(el, 'confirm', async (result) => {
+          if(result) resolve(result);
+          if(data.animate){
+            el.divPN.dataset.bimpnAnimate = 'end';
+            await delay(180);
+          }
+          document.body.removeChild(el.divPN);
+        });
+      });
+    };
   };
-}
 
-const bimPN  = new BimPN();
+  return new BimPushNotifications();
+})();
